4dfe4df73e4fad623fe9e5913980adb42ce2a1bf
Jeni <jenipdang@yahoo.com>
2021-01-23 04:13:51 -0800

    commit tree 9d665be32436653721095f6eb015ebf4c1f1ef4b

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |name, index|
+    puts "#{index + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(callings)# code an argument here
   # Your code here
+  callings.collect do |call|
+    call.capitalize + "!"
+  end 
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_planeteer_calls)# code an argument here
   # Your code here
+  answer = false
+  long_planeteer_calls.each do |call|
+    if call.size > 4
+      answer = true
+    end
+  end
+    answer
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheese)# code an argument here
+  cheese_types = ["cheddar", "gouda", "camembert"]  # the array below is here to help
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
